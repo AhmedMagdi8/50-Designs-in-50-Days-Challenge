6c4ac37424a2a35f374125ad2248f1ce8d4a6de1
ahmedmagdi8 <amagdi825@gmail.com>
2024-10-22 13:43:47 +0300

    Finished Day 24

---
+++ 24 - Content Placeholder/script.js
@@ -0,0 +1,28 @@
+const header = document.getElementById('header')
+const title = document.getElementById('title')
+const excerpt = document.getElementById('excerpt')
+const profile_img = document.getElementById('profile_img')
+const name = document.getElementById('name')
+const date = document.getElementById('date')
+
+const animated_bgs = document.querySelectorAll('.animated-bg')
+const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
+
+
+
+setTimeout(getData, 2500)
+
+function getData() {
+  header.innerHTML =
+    '<img src="https://plus.unsplash.com/premium_photo-1681702156223-ea59bfbf1065?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />'
+  title.innerHTML = 'Lorem ipsum dolor sit amet'
+  excerpt.innerHTML =
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis'
+  profile_img.innerHTML =
+    '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />'
+  name.innerHTML = 'Ahmed Madgy'
+  date.innerHTML = 'Jun 06, 2024'
+
+  animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'))
+  animated_bg_texts.forEach((bg) => bg.classList.remove('animated-bg-text'))
+}
